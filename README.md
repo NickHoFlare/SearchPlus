670f733e8cf34d83de3d1f278a2eea849cdfaf08
Nicholas Ho <terran324@gmail.com>
2021-05-20 15:49:51 +1000

    Add support for environment variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SearchPlus
 Search engines as a Service!
+
+A .env file is required to provide environment variables for the value of PORT number for the API, as well as the various API keys required for the downstream services e.g. Bing, Google.
--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { indexRouter } from './src/routes';
 import { testRouter } from './src/routes/test';
+import dotenv from 'dotenv';
 
-const PORT = 9000;
+// Configure dotenv for .env environment variable support
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -11,6 +15,6 @@ app.use(express.json());
 app.use('/', indexRouter)
 app.use('/test', testRouter);
 
-app.listen(PORT, () => {
-    console.log(`The application is listening on port ${PORT}!`);
+app.listen(port, () => {
+    console.log(`The application is listening on port ${port}!`);
 })
