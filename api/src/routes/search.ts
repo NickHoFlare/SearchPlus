9f2cc75d1ca6e60721492a4108ea7c0b77c704d3
Nicholas Ho <terran324@gmail.com>
2021-06-09 21:00:43 +1000

    Add cache control no cache

--- api/src/routes/search.ts
+++ api/src/routes/search.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import dotenv from 'dotenv';
 import { IBingResponse, IGoogleResponse, IDuckDuckGoResponse } from '../types';
 import { mapResponses } from '../mappers/mapper';
@@ -19,7 +19,7 @@ const invokeAndTimeSearch = async (request: Promise<AxiosResponse>): Promise<IRe
     const response = await request;
     const msElapsed = Date.now() - startTime;
 
-    const responseAndDuration: IResponseAndDuration = {response: response, duration: msElapsed}
+    const responseAndDuration: IResponseAndDuration = { response: response, duration: msElapsed }
     return responseAndDuration;
 }
 
@@ -27,18 +27,22 @@ searchRouter.get("/", async (req, res) => {
     const searchTerms = req.query.query;
 
     try {
-        // TODO: May have to implement header for cache-control: no-store, max-age: 0 to obtain accurate timings
-        const bingRequest = axios.get<IBingResponse>(`${baseUrl}/bing?query=${searchTerms}`);
-        const googleRequest = axios.get<IGoogleResponse>(`${baseUrl}/google?query=${searchTerms}`);
-        const duckDuckGoRequest = axios.get<IDuckDuckGoResponse>(`${baseUrl}/duckduckgo?query=${searchTerms}`);
-    
+        // implement header for cache-control: no-store, max-age: 0 to obtain accurate timings
+        const requestHeaders: AxiosRequestConfig = { 
+            headers: { 'Cache-Control': 'no-cache,max-age=0' }
+        }
+
+        const bingRequest = axios.get<IBingResponse>(`${baseUrl}/bing?query=${searchTerms}`, requestHeaders);
+        const googleRequest = axios.get<IGoogleResponse>(`${baseUrl}/google?query=${searchTerms}`, requestHeaders);
+        const duckDuckGoRequest = axios.get<IDuckDuckGoResponse>(`${baseUrl}/duckduckgo?query=${searchTerms}`, requestHeaders);
+
         const bingResponse = await invokeAndTimeSearch(bingRequest);
         const googleResponse = await invokeAndTimeSearch(googleRequest);
         const duckDuckGoResponse = await invokeAndTimeSearch(duckDuckGoRequest);
 
         const mappedResponses = mapResponses([bingResponse.response, googleResponse.response, duckDuckGoResponse.response],
             [bingResponse.duration, googleResponse.duration, duckDuckGoResponse.duration]);
-            
+
         res.send(mappedResponses);
     } catch (err) {
         res.json(err);
